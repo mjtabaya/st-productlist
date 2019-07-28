aa59dad834d55950225ea9c807b51c71e43bf105
mjtabaya <mjtabaya@gmail.com>
2019-07-28 19:01:56 +0800

    Refactor controller and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :brand
+
+  scope :cheap_products, -> { where(price: 0..50.0).order(price: :asc)}
+  scope :expensive_products, -> { where(price: 50.01..Float::INFINITY).order(price: :asc)}
 end
